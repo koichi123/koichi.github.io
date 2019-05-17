a8435b5cb7e6f8728ab8b34f958127ba5bb541e6
koichi123 <koichi.ehara@gmail.com>
2019-05-17 17:16:51 +0900

    Update serviceworker.js
    modify

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -51,24 +51,42 @@ self.addEventListener('fetch', function(event) {
 });
 
 // プッシュ通知を受け取る
-self.addEventListener("push", function(event) {
-    console.log("Push Notification Recieved", event);
-    if (Notification.permission == "granted") {
-        event.waitUntil(self.registration.showNotification(
-            "受信しました", {
-                body: "お知らせです。",
-                icon: "../images/icon.jpg"
-        }).then(function(showEvent) {}, function(error) {
-                console.log(error);
-            }
-        ));
-    }
-});
+// self.addEventListener("push", function(event) {
+//     console.log("Push Notification Recieved", event);
+//     if (Notification.permission == "granted") {
+//         event.waitUntil(self.registration.showNotification(
+//             "受信しました", {
+//                 body: "お知らせです。",
+//                 icon: "../images/icon.jpg"
+//         }).then(function(showEvent) {}, function(error) {
+//                 console.log(error);
+//             }
+//         ));
+//     }
+// });
 
 // プッシュ通知をクリック
-self.addEventListener("notificationclick", function(event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow("https://koichi123.github.io/koichi.github.io/")
-    );
+// self.addEventListener("notificationclick", function(event) {
+//     event.notification.close();
+//     event.waitUntil(
+//         clients.openWindow("https://koichi123.github.io/koichi.github.io/")
+//     );
+// });
+
+importScripts('https://www.gstatic.com/firebasejs/5.5.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.4/firebase-messaging.js');
+
+firebase.initializeApp({
+    'messagingSenderId': '378984511996'
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+    return self.registration.showNotification(notificationTitle, notificationOptions);
 });
